3093dd5de7078b77a75bdf7a60e759e77ffe70dd
kitsugo <hayashi.jiro@tuta.io>
2024-01-01 14:13:34 +0100

    Tablet + Wayland update

--- .dotfiles/tablet/tablet_init.sh
+++ .dotfiles/tablet/tablet_init.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-# Create configuration files for programs which require access to tablet-specific device events which may not consistently appear with /dev/input/by-path/
+# Create configuration files (initilization) for programs which require access to tablet-specific device events which may not consistently appear with /dev/input/by-path/
 
 # Read local environment variables of device names and then fetch respective event path via 'libinpiut list-devices'
 device_list=$(libinput list-devices)
 switch_mode_event=$(echo "$device_list" | grep -A 1 "$U_TABLET_SWITCH_DEV" | awk 'FNR == 2 {print $2}')
 touch_event=$(echo "$device_list" | grep -A 1 "$U_TOUCH_DEV" | awk 'FNR == 2 {print $2}')
 
-# Write device for watch_tablet into the first line and device for lisgd into the second line of watch_tablet.yml config
+# Write device event for watch_tablet into the first line and device event for lisgd into the second line of ./configs/watch_tablet.yml config.
+# Both programs will read their respective lines on startup
 printf "input_device: %s\n# %s\n" "$switch_mode_event" "$touch_event" | cat - "$HOME"/.dotfiles/tablet/configs/watch_tablet.yml >/tmp/watch_tablet.yml
+# Create a symlink to the config in /tmp/ inside the regular ~/.config location
 [ ! -h "$HOME/.config/watch_tablet.yml" ] && ln -s "/tmp/watch_tablet.yml" "$HOME/.config/watch_tablet.yml"
 
-# Start watch_tablet with the config in place
+# Start watch_tablet with the config in place. It will start all other touch-related programs when table mode is entered
 watch_tablet &
--- .dotfiles/tablet/tablet_util.sh
+++ .dotfiles/tablet/tablet_util.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Utility functions for touchscreen setup
+# Utility commands for touchscreen setup
+# These are called externally by touch-related programs to trigger certain behavior
 
 case "$1" in
-"toggle-keyboard") # Toggle virtual keybord
+"toggle_keyboard") # Toggle virtual keybord
 	kb_visible=$(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible)
 	if [ "$kb_visible" = "b false" ]; then
 		busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
@@ -10,16 +11,16 @@ case "$1" in
 		busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
 	fi
 	;;
-"toggle-menu") # Toggle wofi menu. Must be run with -n to avoid occasional crashing
-	menu_open=$(pgrep "wofi")
-	if [ -z "$menu_open" ]; then
-		wofi -n --style "$HOME/.config/wofi/nightfox.css" --show drun
+"toggle_menu") # Toggle rofi menu. Must be run with -n to avoid occasional crashing
+	menu_opened=$(pgrep "rofi")
+	if [ -z "$menu_opened" ]; then
+		rofi -n --style -show drun
 	else
-		killall "wofi"
+		killall "rofi"
 	fi
 	;;
-"lisgd") # Wrapper to start lisgd with correct flags to make it compilation-agnostic. Reads second line of watch_tablet.yml to determine device event
+"lisgd") # Wrapper to start lisgd with correct flags to make it compilation-agnostic. It reads the second line of watch_tablet.yml to determine device event on which to listen
 	event="$(sed -n '2p' /tmp/watch_tablet.yml 2>/dev/null)"
-	lisgd -d "${event:2}" -g "2,RL,*,*,R,swaymsg workspace next" -g "2,LR,*,*,R,swaymsg workspace prev" -g "3,RL,*,*,R,move_window.sh -1" -g "3,LR,*,*,R,move_window.sh 1" -g "3,UD,*,*,R,swaymsg bar mode toggle touch-bar" -g "3,DU,*,*,R,tablet_util.sh toggle-keyboard" &
+	lisgd -d "${event:2}" -g "2,RL,*,*,R,swaymsg workspace next" -g "2,LR,*,*,R,swaymsg workspace prev" -g "3,RL,*,*,R,move_window.sh -1" -g "3,LR,*,*,R,move_window.sh 1" -g "3,UD,*,*,R,swaymsg bar mode toggle touch-bar" -g "3,DU,*,*,R,tablet_util.sh toggle_keyboard" &
 	;;
 esac
